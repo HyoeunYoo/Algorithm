bd73c6e8973b2ad35f42b185f14066957e187959
HyoeunYoo <yuuhe0913@gmail.com>
2024-03-16 04:22:56 +0900

    add [백준] 18870번 - 좌표 압축 (실버 2)

---
+++ boj/18870_1.py
@@ -0,0 +1,22 @@
+# [백준] 18870번 - 좌표 압축 (실버 2)
+# 2024.03.16 - 143264 KB / 1848 ms
+# [O] 정렬 / 값 / 좌표 압축
+
+import sys
+input = sys.stdin.readline
+
+# 입력
+N = int(input())
+list1 = list(map(int, input().split()))
+list2 = list(set(list1))
+
+# 정렬
+list2.sort()
+
+# {좌표: list2에서의 인덱스} 딕셔너리
+dic = {}    
+for idx, x in enumerate(list2):
+    dic[x] = idx
+
+for x in list1:
+    print(dic.get(x), end = ' ')
